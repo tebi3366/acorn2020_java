bcc2b78bb0e5311f1b8be108f209acda02f98d79
leesanghun <tebi3366@gmail.com>
2020-06-09 12:32:47 +0900

    addd

--- Step11_Interface/src/test/mypac/Calculator.java
+++ Step11_Interface/src/test/mypac/Calculator.java
@@ -1,7 +1,8 @@
 package test.mypac;
 
-@FunctionalInterface
+@FunctionalInterface //메소드 하나만 정의하게 만듬 @fun 컨트롤 스페이스
 public interface Calculator {
 	//인자로 전달되는 두 실수값을 연산해서 결과를 리턴해주는 메소드 
 	public double exec(double a, double b);
 }
+
