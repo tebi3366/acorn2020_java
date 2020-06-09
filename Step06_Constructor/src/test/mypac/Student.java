69add7b00afacd8c7d44cd816865d24b51178bf6
leesanghun <tebi3366@gmail.com>
2020-06-09 12:32:49 +0900

    add

--- Step06_Constructor/src/test/mypac/Student.java
+++ Step06_Constructor/src/test/mypac/Student.java
@@ -7,7 +7,7 @@ package test.mypac;
  *  3. 객체를 생성할때(new 할때) 호출된다. 
  *  4. 객체를 생성하는 시점에 무언가 준비 작업을 할때 유용하다.
  *  5. 생성자를 명시적으로 정의 하지 않아도 기본 생성자는 있다고 간주된다. 
- */
+ */  
 public class Student {
 	//생성자
 	public Student() {
@@ -21,3 +21,4 @@ public class Student {
 		System.out.println("학교를 가요");
 	}
 }
+
