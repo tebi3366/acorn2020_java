300a8a1499db08e5257faf1ceebf9d5641e4fa6a
leesanghun <tebi3366@gmail.com>
2020-06-24 16:04:51 +0900

    Memo 프레임 추가

---
+++ Step19_JDBC/src/test/dao/MemoDao.java
@@ -0,0 +1,146 @@
+package test.dao;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import test.dto.MemoDto;
+import test.util.DBConnect;
+
+public class MemoDao {
+	private static MemoDao dao;
+	
+	private MemoDao() {}
+	
+	public static MemoDao getInstance() {
+		if(dao==null) {
+			dao=new MemoDao();
+		}
+		return dao;
+	}
+	
+	public List<MemoDto> getList(){
+		List<MemoDto> list=new ArrayList<>();
+		
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		try {
+			conn=new DBConnect().getConn();
+			String sql="SELECT num,content,regdate"
+					+ " FROM memo"
+					+ " ORDER BY num ASC";
+			pstmt=conn.prepareStatement(sql);
+			rs=pstmt.executeQuery();
+			while(rs.next()) {
+				int num=rs.getInt("num");
+				String content=rs.getString("content");
+				Date regdate=rs.getDate("regdate");
+				MemoDto dto=new MemoDto();
+				dto.setNum(num);
+				dto.setContent(content);
+				dto.setDate(regdate);
+				
+				list.add(dto);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs!=null)rs.close();
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			}catch(Exception e) {}
+		}
+		return list;
+	}
+	public boolean delete(int num) {
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		int flag=0;
+		try {
+			conn=new DBConnect().getConn();
+			String sql="DELETE FROM memo"
+					+ " WHERE num=?";
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			flag=pstmt.executeUpdate();
+			System.out.println("삭제 했습니다.");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			}catch(Exception e) {}
+		}
+		if(flag>0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	public boolean insert(MemoDto dto) {
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		int flag=0;
+		try {
+			conn=new DBConnect().getConn();
+			String sql="INSERT INTO memo"
+					+ " (num, content, regdate)"
+					+ " VALUES(memo_seq.NEXTVAL, ?, SYSDATE)";
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, dto.getContent());
+			
+			// sql 문을 수행하고 변화된  row 의 갯수를 리턴 받는다. (1)
+			flag=pstmt.executeUpdate();
+			System.out.println("회원 정보를 추가 했습니다.");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			}catch(Exception e) {}
+		}
+		if(flag>0) {
+			return true; //작업 성공이라는 의미에서 true  를 리턴한다.
+		}else {
+			return false; //작업 실패라는 의미에서 false  를 리턴한다.
+		}
+	}
+	public boolean update(MemoDto dto) {
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		int flag=0;
+		try {
+			conn=new DBConnect().getConn();
+			String sql="UPDATE memo"
+					+ " SET content=?, regdate=?"
+					+ " WHERE num=?";
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, dto.getContent());
+			pstmt.setDate(2, dto.getDate());
+			pstmt.setInt(3, dto.getNum());
+			//update 된 row 의 갯수가 반환 된다. 
+			flag=pstmt.executeUpdate();
+			System.out.println("회원 정보를 수정했습니다.");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			}catch(Exception e) {}
+		}
+		if(flag>0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+}
---
+++ Step19_JDBC/src/test/dto/MemoDto.java
@@ -0,0 +1,38 @@
+package test.dto;
+
+import java.sql.Date;
+
+public class MemoDto {
+	//회원 한명의 정보를 담을 필드 선언
+	private int num;
+	private String content;
+	private Date date;
+	//default 생성자 만들기
+	public MemoDto() {}
+	//인자로 필드에 저장할 값을 전달 받는 생성자
+	public MemoDto(int num, String content, Date date) {
+		super();
+		this.num = num;
+		this.content = content;
+		this.date = date;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+}
